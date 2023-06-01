eab1051ebdee3bfbda4a85d04d96e9738d88a999
saman.cxll <129869732+GofurovSamandar@users.noreply.github.com>
2023-06-01 17:34:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,116 @@
+let cards =document.querySelectorAll('.card')
+
+cards[0].addEventListener("click",function(){
+    cards[0].classList.add("activ")
+
+    setTimeout(() => {
+         cards[0].innerHTML=`<i class="fa-brands fa-twitter"></i>`
+         
+       
+})
+    }, 500);
+    cards[2].addEventListener("click",function(){
+        cards[2].classList.add("activ")
+  
+        setTimeout(() => {
+             cards[2].innerHTML=`<i class="fa-brands fa-twitter"></i>`
+             
+           
+    })
+        }, 500);
+        cards[1].addEventListener("click",function(){
+            cards[1].classList.add("activ")
+            setTimeout(() => {
+                 cards[1].innerHTML=`<i class="fa-brands fa-tiktok"></i>`
+        })
+            }, 500);
+            cards[3].addEventListener("click",function(){
+                cards[3].classList.add("activ")
+                setTimeout(() => {
+                     cards[3].innerHTML=`<i class="fa-brands fa-tiktok"></i>`
+            })
+                }, 500);
+
+                cards[4].addEventListener("click",function(){
+                    cards[4].classList.add("activ")
+                    setTimeout(() => {
+                         cards[4].innerHTML=`<i class="fa-solid fa-house"></i> `
+                })
+                    },500);
+                    cards[6].addEventListener("click",function(){
+                        cards[6].classList.add("activ")
+                        setTimeout(() => {
+                             cards[6].innerHTML=`<i class="fa-solid fa-house"></i> `
+                    })
+                        },500);
+                        cards[5].addEventListener("click",function(){
+                            cards[5].classList.add("activ")
+                            setTimeout(() => {
+                                cards[5].innerHTML=`<i class="fa-solid fa-magnifying-glass"></i>`
+                                
+                            }, 500);
+                        })
+                        cards[7].addEventListener("click",function(){
+                            cards[7].classList.add("activ")
+                            setTimeout(() => {
+                                cards[7].innerHTML=`<i class="fa-solid fa-magnifying-glass"></i>`
+                                
+                            }, 500);
+                        })
+                        cards[8].addEventListener("click",function(){
+                            cards[8].classList.add("activ")
+                            setTimeout(() => {
+                                cards[8].innerHTML=`<i class="fa-solid fa-user"></i>`
+                                
+                            }, 600);
+                        })
+                        cards[10].addEventListener("click",function(){
+                            cards[10].classList.add("activ")
+                            setTimeout(() => {
+                                cards[10].innerHTML=`<i class="fa-solid fa-user"></i>`
+                                
+                            }, 600);
+                        })
+                        cards[11].addEventListener("click",function(){
+                            cards[11].classList.add("activ")
+                            setTimeout(() => {
+                                cards[11].innerHTML=`<i class="fa-brands fa-facebook"></i>`
+                                
+                            }, 600);
+                        })
+                        cards[9].addEventListener("click",function(){
+                            cards[9].classList.add("activ")
+                            setTimeout(() => {
+                                cards[9].innerHTML=`<i class="fa-brands fa-facebook"></i>`
+                                
+                            }, 600);
+                        })
+
+                        cards[12].addEventListener("click",function(){
+                            cards[12].classList.add("activ")
+                            setTimeout(() => {
+                                cards[12].innerHTML=`<i class="fa-solid fa-download"></i>`
+                                
+                            }, 600);
+                        })
+                        cards[14].addEventListener("click",function(){
+                            cards[14].classList.add("activ")
+                            setTimeout(() => {
+                                cards[14].innerHTML=`<i class="fa-solid fa-download"></i>`
+                                
+                            }, 600);
+                        })
+                        cards[13].addEventListener("click",function(){
+                            cards[13].classList.add("activ")
+                            setTimeout(() => {
+                                cards[13].innerHTML=`<i class="fa-brands fa-instagram"></i>`
+                                
+                            }, 600);
+                        })
+                        cards[15].addEventListener("click",function(){
+                            cards[15].classList.add("activ")
+                            setTimeout(() => {
+                                cards[15].innerHTML=`<i class="fa-brands fa-instagram"></i>`
+                                
+                            }, 600);
+                        })
